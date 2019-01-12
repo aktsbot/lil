ffab7d06829077992a2e7dc410a13c1febccfa44
akts <box.ashishk@gmail.com>
2019-01-12 22:42:43 +0530

    verifyToken function add

--- utils.js
+++ utils.js
@@ -1,3 +1,5 @@
+const jwt = require("jsonwebtoken");
+
 const respJSON = data => {
   return {
     err: data.err,
@@ -14,7 +16,27 @@ const respDefaultErrorJSON = () => {
   };
 };
 
+const verifyToken = async (req, res, next) => {
+  const token = req.headers["x-access-token"];
+  if (!token)
+    return res.status(403).json({ error: true, msg: "No token provided." });
+
+  jwt.verify(token, process.env.JWT_SECRET || "00secret00", (err, decoded) => {
+    if (err)
+      return res
+        .status(500)
+        .json({ error: true, msg: "Failed to authenticate token." });
+
+    // if everything good, save to request for use in other routes
+    req.token = {
+      userId: decoded.id
+    };
+    next();
+  });
+};
+
 module.exports = {
   respJSON,
-  respDefaultErrorJSON
+  respDefaultErrorJSON,
+  verifyToken
 };
